3f813e8d1f06aec440f5da229932d849f3067d83
matthijsk16 <matthijs.klasens@gmail.com>
2024-08-06 16:09:52 +0200

    switch to flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+
+  description = "NixOS and Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      mkSystem = packages: system: hostname:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { 
+            inherit inputs; 
+            pkgs = import packages { 
+              inherit system; 
+              config = { allowUnfree = true; }; 
+            };
+          };
+          modules = [
+            { networking.hostName = hostname; }
+            ./modules/system/configuration.nix
+            ./hosts/${hostname}/configuration.nix
+          ];
+        };
+
+    in {
+    nixosConfigurations = {
+      bois-server = mkSystem nixpkgs "x86_64-linux" "bois-server";
+    };
+  };
+
+}
---
+++ modules/system/configuration.nix
@@ -0,0 +1,82 @@
+
+{ config, pkgs, inputs, ... }:
+
+{
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+      trusted-users = [ "@wheel" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  boot = {
+    tmp.cleanOnBoot = true;
+    loader = {
+      timeout = 5;
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+      };
+    };
+  };
+
+  # Remove unnecessary packages
+  environment.defaultPackages = [];
+  services.xserver.desktopManager.xterm.enable = false;
+
+  environment.shells = [ pkgs.bash ];
+  users.defaultUserShell = pkgs.bash;
+  # programs.zsh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    neovim  # EDITOR
+    bat     # cat with wings
+    wget    # download files
+    zoxide  # cd with superpowers
+    git     # version control
+  ];
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
