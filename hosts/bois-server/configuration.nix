a950226cad102eae484c567e1a52af9e4a510268
matthijsk16 <matthijs.klasens@gmail.com>
2024-08-06 19:52:19 +0200

    Added cloudflared package

--- hosts/bois-server/configuration.nix
+++ hosts/bois-server/configuration.nix
@@ -24,7 +24,9 @@
     isNormalUser = true;
     description = "De Bois";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    packages = with pkgs; [
+      cloudflared
+    ];
   };
 
   # List services that you want to enable:
