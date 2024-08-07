73915c63ad88d0b1e069907c4e972f624c89ca0c
matthijsk16 <matthijs.klasens@gmail.com>
2024-08-07 12:07:12 +0200

    Epidode II: A new hope

--- modules/pelican-panel/package.nix
+++ modules/pelican-panel/package.nix
@@ -27,7 +27,7 @@ php83.buildComposerProject (finalAttrs: {
   };
 
   # The composer vendor hash
-  # vendorHash = "sha256-86s/F+/5cBAwBqZ2yaGRM5rTGLmou5//aLRK5SA0WiQ=";
+  vendorHash = "sha256-86s/F+/5cBAwBqZ2yaGRM5rTGLmou5//aLRK5SA0WiQ=";
 
   # If the composer.lock file is missing from the repository, add it:
   # composerLock = ./path/to/composer.lock;
