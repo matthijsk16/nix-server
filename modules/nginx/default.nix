2693124a8f7caf3c29b8c6eb7a3a0537cc2e034a
matthijsk16 <matthijs.klasens@gmail.com>
2024-08-07 17:13:55 +0200

    Added basic NGINX

--- modules/nginx/default.nix
+++ modules/nginx/default.nix
@@ -12,7 +12,7 @@ in
     # Open ports in the firewall.
     networking.firewall.allowedTCPPorts = [ 80 443 ];
     # networking.firewall.allowedUDPPorts = [ ... ];
-    systemd.services.nginx.serviceConfig.ProtectHome = "read-only";
+    # systemd.services.nginx.serviceConfig.ProtectHome = "read-only";
     # users.users.bois.homeMode = 750;
     services.nginx = {
       enable = true;
@@ -21,7 +21,6 @@ in
         "kaasbois.nl" = {
           # addSSL = true;
           # enableACME = true;
-          # root = "/home/bois/nix-server/modules/nginx/";
           root = "/var/www/kaasbois.nl/";
         };
       };
