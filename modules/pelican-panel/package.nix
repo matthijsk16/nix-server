771b110e0a0e279d439ad9df1d1e2fccd6327818
matthijsk16 <matthijs.klasens@gmail.com>
2024-08-07 14:28:40 +0200

    Epidode II: A new hope

--- modules/pelican-panel/package.nix
+++ modules/pelican-panel/package.nix
@@ -1,35 +1,32 @@
-{ lib, php83, fetchFromGitHub, fpm }:
+{ lib, php82, fetchFromGitHub }:
 
-php83.buildComposerProject (finalAttrs: {
+php82.buildComposerProject (finalAttrs: {
   pname = "ppelican-panel";
   version = "v1.0.0-beta7";
 
   src = fetchFromGitHub {
     owner = "pelican-dev";
     repo = "panel";
-    rev = finalAttrs.version;  # v1.0.0-beta7
+    rev = finalAttrs.version;
     hash = "sha256-h0+6rDy7U24XEdqAnfd8Q77VE8Ji2llSxy/vQawPQw4=";
   };
 
-  php83 = php83.buildEnv {
+  php82 = php82.buildEnv {
     extensions = ({ enabled, all }: enabled ++ (with all; [
       gd
       mysqli
-      mbstring
+      mbstrin
       bcmath
       xml
       curl
       zip
       intl
       sqlite3
-      # fpm
     ]));
   };
 
   # The composer vendor hash
-  # vendorHash = "sha256-86s/F+/5cBAwBqZ2yaGRM5rTGLmou5//aLRK5SA0WiQ=";
-  vendorHash = "sha256-h0+6rDy7U24XEdqAnfd8Q77VE8Ji2llSxy/vQawPQw4=";
-  # vendorHash = lib.fakeSha256;
+  vendorHash = lib.fakeHash;
 
   # If the composer.lock file is missing from the repository, add it:
   # composerLock = ./path/to/composer.lock;
